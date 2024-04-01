20087d49c88e1c1f771fa142542dafc9d8a7ec6a
eduardo-felix <duduflj@gmail.com>
2024-04-01 16:56:11 -0300

    Coments removed

--- force-app/main/default/lwc/weatherInfo/weatherInfo.js
+++ force-app/main/default/lwc/weatherInfo/weatherInfo.js
@@ -3,7 +3,6 @@ import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import getWeatherData from '@salesforce/apex/WeatherAPI.getWeatherData';
 
-// Definir os campos para recuperar do registro atual
 const fields = ['Account.BillingCity'];
 
 export default class WeatherComponent extends LightningElement {
@@ -12,8 +11,7 @@ export default class WeatherComponent extends LightningElement {
     description;
     iconUrl;
     city;
-
-    // Wire para buscar o registro atual e o campo BillingCity
+   
     @wire(getRecord, { recordId: '$recordId', fields })
     wiredRecord({ error, data }) {
         if (data) {
@@ -24,12 +22,11 @@ export default class WeatherComponent extends LightningElement {
         }
     }
 
-    // Método para buscar os dados do clima usando o método Apex
-    fetchWeatherData(cityName) {
+       fetchWeatherData(cityName) {
         getWeatherData({ cityName })
             .then(result => {
                 const data = JSON.parse(result);
-                this.temperature = (data.main.temp - 273.15).toFixed(2); // Convertendo de Kelvin para Celsius
+                this.temperature = (data.main.temp - 273.15).toFixed(2);
                 this.description = data.weather[0].description;
                 this.city = cityName;
                 const iconCode = data.weather[0].icon;
